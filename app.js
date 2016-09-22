6a46523d8ec4dd394bca231e791fa188ef2ed7c4
Sylver Yagi <silvery4543@gmail.com>
2016-09-22 10:09:27 +1000

    adding a new user to db and testing again

--- app.js
+++ app.js
@@ -18,8 +18,9 @@ var express 						= require('express'),
 // ================================================= //
 //               mongoose configurations             //
 // ================================================= //
-var dbUri = 'mongodb://127.0.0.1:27017/lra'; // local - dev
+// var dbUri = 'mongodb://127.0.0.1:27017/lra'; // local - dev
 // var dbUri = 'mongodb://reader_loopcount_db:readloopcount@ds019980.mlab.com:19980/heroku_gt6n53cm';
+var dbUri = 'mongodb://test_reader:default@ds019980.mlab.com:19980/heroku_gt6n53cm';
 
 mongoose.connect(dbUri);
 var conn = mongoose.connection;

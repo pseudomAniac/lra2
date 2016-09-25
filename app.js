963ca9f53d071e3867708e9b593d8fc0d620d007
Sylver Yagi <silvery4543@gmail.com>
2016-09-26 09:17:53 +1000

    added google analytics

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-var express 						= require('express'),
-		xray								= require('x-ray')(),
-		authorsController 	= require('./server/controllers/authors-controller'),
-		articlesController 	= require('./server/controllers/articles-server-controller'),
-		Article 						= require('./server/models/article-model'),
-		bodyParser 					= require('body-parser'),
-		mongoose						= require('mongoose'),
-		Q 									= require('q'),
-		passport = require('passport'),
-		localPass = require('passport-local'),
-//		config 								= require('./app/config.js'),
-		myConf								= require("./app/my-conf.js"),
-//		Lockit								= require('lockit'),
-		cookieSession						= require('cookie-session'),
-		cookieParser						= require('cookie-parser'),
-		app 								= express();
+var express  = require('express'),
+	xray = require('x-ray')(),
+	authorsController 	= require('./server/controllers/authors-controller'),
+	articlesController 	= require('./server/controllers/articles-server-controller'),
+	Article  = require('./server/models/article-model'),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	Q  			= require('q'),
+	passport = require('passport'),
+	localPass = require('passport-local'),
+	serveStatic = require("serve-static"),
+//	config = require('./app/config.js'),
+	myConf = require("./app/my-conf.js"),
+//	Lockit = require('lockit'),
+	cookieSession = require('cookie-session'),
+	cookieParser = require('cookie-parser'),
+	app = express();
 		
 // ================================================= //
 //               mongoose configurations             //
@@ -57,94 +58,92 @@ var	sampleArticle = {
 	};
 
 var counter	= 0;
-
+// get calls
 app.get('/write/:country', function (req, res){
     var country = req.params.country, lurl = 'http://www.looppng.com/section/all?page=', nurl = '';
 	retrieve(country,lurl,25,0);
     res.redirect('/page/'+country);
 });
-
 app.get('/', function (req, res){
 	res.render(__dirname + '/client/views/png');
 	myConf.printDate();
 });
-
 app.get('/home', function(req, res) {
 	res.render(__dirname + '/client/views/index');
 });
-
 // api call to get stories
-app.get('/articles/:country', articlesController.listArticles); 
-
+app.get('/articles/:country', articlesController.listArticles);
+app.get("/test-xray", function (req, res) {
+	xray('http://stackoverflow.com/', {
+		links: [ xray('a.question-hyperlink', '@href') ]
+	})(function(err, obj) {
+		if (err) console.log(err);
+		else if (obj) console.log(obj);
+		else res.send("returned an empty set");
+	});
+});
 // api call to export stories 
 app.get('/export/:country/:startDate', articlesController.exportArticles);
 app.get('/export/:country', articlesController.exportArticles);
-
 // uri call to render pages
 app.get('/page/:country', function (req, res){
 	myConf.printDate();
 	res.render(__dirname + '/client/views/'+req.params.country);
 });
-
 app.get("/page/:country/top5", function (req, res) {
 	myConf.printDate();
 	res.render(__dirname + "/client/views/" + req.params.country + "-top-5");
 });
-
 app.get("/populate", function (req, res) {
 	res.render(__dirname + "/client/views/populate")
 });
-
 app.get("/populate/content/", function (req, res) {
 	var country = req.query.country.toLowerCase(), pages = req.query.pages, counter = req.query.counter;
 	retrieve(country, pages, counter)
 	res.redirect("/page/"+country);
 });
-
 app.get('/delete/:country', function (req, res){
-	var country = req.params.country;
+	var country = req.params.country, numpages = 25, startAt = 0;
 	switch (country) {
 		case 'nauru':
 			naurucollection.drop();
-			retrieve("nauru",25,0);
+			retrieve("nauru",numpages,startAt);
 			break;
 		case 'png':
 			pngcollection.drop();
-			retrieve("png",25,0);
+			retrieve("png",numpages,startAt);
 			break;
 		case 'samoa':
 			samoacollection.drop();
-			retrieve("samoa",25,0);
+			retrieve("samoa",numpages,startAt);
 			break;
 		case 'tonga':
 			tongacollection.drop();
-			retrieve("tonga",25,0);
+			retrieve("tonga",numpages,startAt);
 			break;
 		case 'vanuatu':
 			vanuatucollection.drop();
-			retrieve("vanuatu",25,0);
+			retrieve("vanuatu",numpages,startAt);
 			break;
 		case 'all':
 			naurucollection.drop();
-			retrieve("nauru",25,0);
+			retrieve("nauru",numpages,startAt);
 			pngcollection.drop();
-			retrieve("png",25,0);
+			retrieve("png",numpages,startAt);
 			samoacollection.drop();
-			retrieve("samoa",25,0);
+			retrieve("samoa",numpages,startAt);
 			tongacollection.drop();
-			retrieve("tonga",25,0);
+			retrieve("tonga",numpages,startAt);
 			vanuatucollection.drop();
-			retrieve("vanuatu",25,0);
+			retrieve("vanuatu",numpages,startAt);
 		default:
 			break;
 	}
 	myConf.printDate();
 	res.redirect('/');
 });
-
 // functions
-
-function retrieve(country,pages,counter) {
+function retrieve(country,numpages,counter) {
 	var lurl = "";
 	switch (country) {
 		case 'nauru':
@@ -163,7 +162,7 @@ function retrieve(country,pages,counter) {
 			lurl = 'http://www.looppng.com/section/all?page=' + counter;
 	}
 	// generate links to source publication data from
-	for (var i=counter; i<counter+pages; i++) {
+	for (var i=counter; i<counter+numpages; i++) {
 		var nurl = lurl+i;
 		console.log(country,'info collection',i);
 		xray(nurl,{
@@ -172,35 +171,35 @@ function retrieve(country,pages,counter) {
 			if (!err) {
 				obj.links.forEach(function (link) {
 					xray(link.link, postmeta_extract)(function (err, data) {
-						if (!err) {
-							// clean before saving: category
-							var tmpCat = data.category.split(" ");
-							data.category = tmpCat[tmpCat.length - 1].replace("taxonomy-", "");
-							// clean before saving: date published
-							var tmpPubDate = data.pubdate.slice(data.pubdate.search(",") + 2, 100),
-														cleaned = tmpPubDate.slice(0, tmpPubDate.search(",") + 6);
-							data.pubdate = cleaned.replace(",", "");
-							// clean before saving: views
-							var rawViews = data.views.replace("\n", "");
-							data.views = rawViews.split(" ")[6];
-							switch (country) {
-								case 'nauru':
-									insertNauruArticle(data);
-									break;
-								case 'png':
-									insertPNGArticle(data);
-									break;
-								case 'samoa':
-									insertSamoaArticle(data);
-									break;
-								case 'tonga':
-									insertTongaArticle(data);
-									break;
-								default:
-									insertVanuatuArticle(data);
-									break;
-							}
+					 if (!err) {
+						// clean before saving: category
+						var tmpCat = data.category.split(" ");
+						data.category = tmpCat[tmpCat.length - 1].replace("taxonomy-", "");
+						// clean before saving: date published
+						var tmpPubDate = data.pubdate.slice(data.pubdate.search(",") + 2, 100),
+							cleaned = tmpPubDate.slice(0, tmpPubDate.search(",") + 6);
+						data.pubdate = cleaned.replace(",", "");
+						// clean before saving: views
+						var rawViews = data.views.replace("\n", "");
+						data.views = rawViews.split(" ")[6];
+						switch (country) {
+							case 'nauru':
+								insertNauruArticle(data);
+								break;
+							case 'png':
+								insertPNGArticle(data);
+								break;
+							case 'samoa':
+								insertSamoaArticle(data);
+								break;
+							case 'tonga':
+								insertTongaArticle(data);
+								break;
+							default:
+								insertVanuatuArticle(data);
+								break;
 						}
+					 }
 					});
 				});
 			}
@@ -208,7 +207,7 @@ function retrieve(country,pages,counter) {
 	}
 	//return callback("done")
 }
-
+// app.use & app.set codes
 app.set('views',__dirname + '/client/views');
 app.set("view engine",'ejs');
 app.use('/css', express.static(__dirname + '/public/css'))
@@ -220,7 +219,8 @@ app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cookieSession({ secret: 'monobelle' }));
-
+app.use(serveStatic("/public"));
+app.use(serveStatic("/public/img"));
 // var lockit = new Lockit(config);
 // app.use(lockit.router);
 /* 

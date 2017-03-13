c173b24b95f94dd0b5adfb21d4b78b691589c836
Sylver Yagi <silvery4543@gmail.com>
2017-03-13 16:58:26 +1000

    1.9.7

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.post("/", function(req, res) {
 	res.redirect("/populate");
 });
 app.get('/', function(req, res) {
-	console.log(app.get("POP_COUNTER - ",POP_COUNTER));
+	console.log(app.get("POP_COUNTER - ",process.env.POP_COUNTER));
 	res.render('articles');
 });
 app.get('/write/:country', function(req, res) {

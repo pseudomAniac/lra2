98e24cf103221cb72ce0c5a42e16e75a1ba254e9
Sylver Yagi <silvery4543@gmail.com>
2017-03-23 09:05:43 +1000

    1.9.10 - GOOGLE ANALYTICS INTEGRATION

--- app.js
+++ app.js
@@ -3,28 +3,76 @@ var express  = require('express'),
 	moment = require('moment'),
 	q = require('q'),
 	articlesController 	= require('./server/controllers/articles-server-controller'),
-	retriever = require("./lib/retriever"),
-	timeout = require("./lib/timeouts"),
-	compression = require("compression"),
-	serveStatic = require("serve-static"),
+	retriever = require('./lib/retriever'),
+	timeout = require('./lib/timeouts'),
+	compression = require('compression'),
+	serveStatic = require('serve-static'),
 	bodyParser = require('body-parser'),
 	cookieSession = require('cookie-session'),
 	cookieParser = require('cookie-parser'),
 	app = express();
-// routes
-var testRouter1 = express.Router();
-testRouter1.get('/test', (req,res)=>{
-	res.send('test 1 succeeded');
+	// google apis, auths & tokens
+var google = require('googleapis'),
+	ga = google.analytics({version:'v3'});
+var opts = { tokens: {} };
+	// auth: JWT
+var authS2S = require('./node_modules/lra2-1e61de3b4a18.json'),
+	jwtClient = new google.auth.JWT(
+		process.env.JWT_CLIENT_EMAIL || authS2S.client_email,
+		null,
+		process.env.JWT_PRIVATE_KEY || authS2S.private_key,
+		process.env.JWT_SCOPE || authS2S.scope,
+		null
+	);
+jwtClient.authorize(function(err,tokens) {
+	if (err) {
+		console.log(err)
+		return;
+	} else {
+		jwtClient.setCredentials(tokens);
+		opts.tokens = tokens;
+	}
+	// console.log('jwtClient',jwtClient);
+	// queryData(ga)
 })
-app.use('/test1',testRouter1);
-var testRouter2 = express.Router();
-testRouter2.get('/test', (req,res)=>{
-	res.send('test 2 succeeded');
+// raw functions
+function queryData(analytics) {
+  analytics.data.ga.get({
+    'auth': jwtClient,
+    'ids': process.env.JWT_VIEW_ID || authS2S.view_id,
+    'metrics': 'ga:pageviews',
+    'dimensions': 'ga:pagePath',
+    'start-date': '7daysAgo',
+    'end-date': 'today',
+    'sort': '-ga:pageviews',
+    'max-results': 10,
+    'filters': 'ga:pagePath=~/aust-and-digicel-support-school-infrastructure-bougainville-55047',
+  }, function (err, response) {
+    if (err) {
+      console.log('\nERROR\n----------\n',err);
+      return;
+    }
+    console.log('\nSUCCESS\n',JSON.stringify(response, null, 4));
+  });  
+}
+// routes: google analytics
+var gaRouter = express.Router();
+gaRouter.get('/oauthcallback?:code',(req,res)=>{
+	var code = req.params.code;
+	console.log('req.params',req.params);
+	queryData(ga);
+	console.log('authentic');
+	res.render('png');
 })
-app.use('/test2',testRouter2);
+gaRouter.get('/test', (req,res)=>{
+	console.log('/test route accessed.')
+	queryData(ga);
+	res.render('search')
+})
+app.use('/ga',gaRouter);
 // http requests
-app.post("/", function(req, res) {
-	res.redirect("/populate");
+app.post('/', function(req, res) {
+	res.redirect('/populate');
 });
 app.get('/', function(req, res) {
 	res.render('articles');
@@ -39,21 +87,21 @@ app.get('/articles/:country', articlesController.listArticles);
 app.get('/page/:country', function(req, res){res.render(req.params.country)});
 // uri call to get top 5 stories
 app.get('/top/:country', articlesController.top5);
-app.get("/top-5/:country", function(req, res) {res.render(req.params.country + "-top-5")});
+app.get('/top-5/:country', function(req, res) {res.render(req.params.country + '-top-5')});
 // api call to export stories
 app.get('/export/:country/:startDate', articlesController.exportArticles);
 app.get('/export/:country', articlesController.exportArticles);
-app.get('/search', (req, res) => {res.render("search")});
+app.get('/search', (req, res) => {res.render('search')});
 app.get('/query/', articlesController.queryArticles);
 app.get('/api/query/', (req,res) => {res.render('query')});
 // api call to populate stories
-app.get("/populate", (req, res) => {res.render('populate')});
-app.get("/populate/content/", function(req, res) {
+app.get('/populate', (req, res) => {res.render('populate')});
+app.get('/populate/content/', function(req, res) {
 	var country = req.query.country.toLowerCase(), pagesToScan = req.query.pages, startScanAt = req.query.counter;
 	retriever.getArticles(country,pagesToScan,startScanAt);
-	res.redirect("/page/"+country);
+	res.redirect('/page/'+country);
 });
-app.get("/populate/auto/", (req, res) => {
+app.get('/populate/auto/', (req, res) => {
 	console.log(req.query);
 	var li = req.query.limit,
 	cy = req.query.country,
@@ -63,7 +111,7 @@ app.get("/populate/auto/", (req, res) => {
 });
 // app.use & app.set codes
 app.set('views',__dirname + '/client/views');
-app.set("view engine",'ejs');
+app.set('view engine','ejs');
 app.use('/fonts', express.static(__dirname + '/public/fonts'));
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
@@ -75,44 +123,13 @@ app.use(bodyParser.json());
 // app.use(cookieParser());
 // app.use(cookieSession({ secret: 'monobelle' }));
 app.use(compression());
-app.use(serveStatic("/public/*/*"));
-app.use(serveStatic("/css/*.*"));
-app.use(serveStatic("/js/*.*"));
-app.use(serveStatic("/img/*.*"));
-app.use(serveStatic("/client/js/*.*"));
-app.use(serveStatic("/server/js/*.*"));
+app.use(serveStatic('/public/*/*'));
+app.use(serveStatic('/css/*.*'));
+app.use(serveStatic('/js/*.*'));
+app.use(serveStatic('/img/*.*'));
+app.use(serveStatic('/client/js/*.*'));
+app.use(serveStatic('/server/js/*.*'));
 app.set('port', (process.env.PORT || 3000));
-app.listen(app.get('port'), function() { console.log("app started at port " + app.get('port')); });
+app.listen(app.get('port'), function() { console.log('app started at port ' + app.get('port')); });
 
 // module.exports = express;
-
-// var dataRet = setInterval(()=>{
-// 	if(globalCounter<globalLimit) {
-// 		retriever.automateDataRetrieval(globalCountry,1,globalCounter);
-// 		globalCounter++;
-// 		console.log(globalCountry,globalCounter,globalLimit)
-// 	} else { 
-// 		console.log(globalCounter,"end reached.");
-// 		clearInterval(dataRet);
-// 		// setInterval(()=>{
-// 		// 	// call fx to check for recent updates to the story links array
-// 		// 	console.log('getUpdate("png")')
-// 		// 	retriever.getUpdate("png");
-// 		// }, (1000*60*15));
-// 		// setInterval(()=>{
-// 		// 	// call fx to check for recent updates to the story links array
-// 		// 	console.log('getUpdate("pacific")')
-// 		// 	retriever.getUpdate("pacific");
-// 		// }, (1000*60*60*2));
-// 	}
-// },(1000*15))
-
-// app.get('/dashboard', function(req, res) {
-// 	res.render('index');
-// });
-// app.get("/force-update/", function(req,res) {
-// 	var country = req.query.country.toLowerCase()
-// 	retriever.getUpdate(country);
-// 	res.redirect('/');
-// 	// res.render(__dirname + "/client/views/articles");
-// });

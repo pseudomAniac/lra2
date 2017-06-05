27156c4134f22c9ff8efd9a3130cda08c9294cd6
Sylver Yagi <silvery4543@gmail.com>
2017-06-05 16:53:24 +1000

    1.9.15

--- server/models/article-model.js
+++ server/models/article-model.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose'),
 		Schema 	 = mongoose.Schema;
 var ArticlesSchema = new Schema({
-	domain: {type: String, index: true},
-	nodeID: {type: Number, index: true},
+	domain: {type: String, index: false},
+	nodeID: {type: Number, index: false},
 	pubdate: {type: Number, index: true},
 	title: String,
 	views: Number,
-	pageviews: Number,
-	link: {type: String, index: true},
+	pageviews: {type: Number, index: true},
+	link: {type: String, index: false},
 	author: String,
 	category: {type: Array},
 	publisher: String,
